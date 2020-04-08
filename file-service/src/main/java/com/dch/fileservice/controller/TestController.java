5a5118de8cf4d628e15f19a01dbc6c2e0146484e
DCH <1148332999@qq.com>
2020-04-08 09:57:50 +0800

    测试通过

--- file-service/src/main/java/com/dch/fileservice/controller/TestController.java
+++ file-service/src/main/java/com/dch/fileservice/controller/TestController.java
@@ -1,16 +1,25 @@
 package com.dch.fileservice.controller;
 
 import com.dch.fileservice.model.ApiResult;
+import com.dch.fileservice.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class TestController {
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/t1")
     public ApiResult t1() {
+        log.warn(Thread.currentThread().getName() + "::::::" + userService.selectById(new Random().nextInt(5) + 1).toString());
         return new ApiResult().setSuccess().setMsg("访问测试：成功！");
     }
 }
